1bcbbd0d1b296fc522ca50d23ff905d0dda45505
k23036967 <k23036967@kcl.ac.uk>
2025-09-29 21:39:08 +0100

    feat: d3 canvas component

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,8 +1,13 @@
+import VisualiserCanvas from "@/components/VisualiserCanvas";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col p-8 justify-center items-center text-stroke">
+      <VisualiserCanvas  className="w-full h-[80vh] rounded-md border-1 border-secondary"> 
+
+
+      </VisualiserCanvas>
     </div>
   );
 }
--- front-end/src/components/VisualiserCanvas.tsx
+++ front-end/src/components/VisualiserCanvas.tsx
@@ -1,12 +1,36 @@
-import * as React from 'react';
+'use client'
+import { useRef, useEffect } from "react"; 
+import * as d3 from "d3"
 
-export interface IAppProps {
+type CanvasProps = {
+    width?: number; 
+    height?: number;
+    className?: string; 
+    children?: React.ReactNode;
 }
 
-export function App (props: IAppProps) {
+const VisualiserCanvas: React.FC<CanvasProps> = ({ 
+  width = 800, 
+  height = 600, 
+  className = "", 
+  children 
+}) => {
+  const svgRef = useRef<SVGSVGElement | null>(null);
+
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
+    svg.selectAll("*").remove(); // optional cleanup if you want "fresh" mounts
+  }, []);
+
   return (
-    <div>
-      
-    </div>
+    <svg
+      ref={svgRef}
+ 
+      className={`${className}`}
+    >
+      <g className="d3-root">{children}</g>
+    </svg>
   );
-}
+};
+
+export default VisualiserCanvas;
