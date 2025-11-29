d13856b992f74de57bc5d1776f96aea3c0f6f90f
k23036967 <k23036967@kcl.ac.uk>
2025-11-29 20:18:26 +0000

    feat: title and webapp metadata

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -7,11 +7,14 @@ export default function Home() {
     tex: { packages: { "[+]": ["color"] } },
   };
   return (
+    <>
+    <p className="text-text p-1 text-2xl font-bold">Conv<span className="text-accent-warm">Sandbox</span>.</p>
     <MathJaxContext config={config}>
       <div className="flex flex-col p-1 justify-center items-center text-stroke">
         {/* Simulator */}
         <Visualiser />
       </div>
     </MathJaxContext>
+    </>
   );
 }
